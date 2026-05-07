99a77e5f1ee0f0469ce05c2cfce25090fdd3dc6d
d-adamian <dmitry.adamian@gmail.com>
2026-05-07 18:55:36 +0300

    Talk to the backend server

--- bookish-app/playwright/bookish.spec.ts
+++ bookish-app/playwright/bookish.spec.ts
@@ -1,6 +1,31 @@
 import { test, expect } from "@playwright/test";
+import axios from "axios";
 
 const BASE_URL = "http://localhost:5173";
+const STUB_SERVER_URL = "http://localhost:8079";
+
+interface BookResponse {
+  name: string;
+  id: number;
+}
+
+const books: BookResponse[] = [
+  { name: "Refactoring", id: 1 },
+  { name: "Domain-driven design", id: 2 },
+  { name: "Microservices", id: 3 },
+];
+
+test.beforeEach(async () => {
+  const booksUrl = `${STUB_SERVER_URL}/books`;
+  const cleanupUrl = `${booksUrl}?_cleanup=true`;
+  await axios.delete(cleanupUrl).catch((err) => err);
+
+  for (const book of books) {
+    await axios.post(booksUrl, book, {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+});
 
 test.describe("Bookish application", () => {
   test("Visits the bookish", async ({ page }) => {
@@ -12,8 +37,9 @@ test.describe("Bookish application", () => {
     await page.goto(BASE_URL);
     await expect(page.getByTestId("book-list")).toBeAttached();
     const bookItems = await page.getByTestId("book-item").all();
-    await expect(bookItems).toHaveLength(2);
+    await expect(bookItems).toHaveLength(3);
     await expect(bookItems[0]).toHaveText("Refactoring");
     await expect(bookItems[1]).toHaveText("Domain-driven design");
+    await expect(bookItems[2]).toHaveText("Microservices");
   });
 });
--- bookish-app/src/App.tsx
+++ bookish-app/src/App.tsx
@@ -1,8 +1,19 @@
 import { Typography } from "@mui/material";
 import { BookList } from "./BookList";
+import type { Book } from "./types";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:8079";
 
 export const App = () => {
-  const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    axios.get(`${SERVER_URL}/books`).then((res) => setBooks(res.data));
+  }, []);
+
   return (
     <>
       <Typography variant="h2" component="h2" data-testid="heading">
